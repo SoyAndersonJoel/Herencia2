bf4079d349ece5c4df682715f8c963eb64cb0049
SoyAndersonJoel <161794137+SoyAndersonJoel@users.noreply.github.com>
2024-05-23 08:21:49 -0500

    class new

--- src/main/java/org/example/Pentagono.java
+++ src/main/java/org/example/Pentagono.java
@@ -1,2 +1,31 @@
-package org.example;public class Pentagono {
+package org.example;
+
+public class Pentagono extends FigurasGeometricas{
+    String nom;
+    int eda;
+
+    public Pentagono(String nombre,String nom, int nLados, int eda) {
+        super(nombre, nLados);
+        this.nom = nom;
+        this.eda = eda;
+    }
+    public Pentagono() {}
+
+
+    public String getNom() {
+        return nombre;
+    }
+
+
+    public void setNom(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEda() {
+        return eda;
+    }
+
+    public void setEda(int eda) {
+        this.eda = eda;
+    }
 }
